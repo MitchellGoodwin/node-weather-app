af712a370c7458c2fa8976663f31ac46152bc515
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-04-28 16:14:33 -0400

    First requests from weatherstack

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=fbb67e0cab01405a82ac597d26c74640&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=fbb67e0cab01405a82ac597d26c74640&query=37.8267,-122.4233&units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    const temp = response.body.current.temperature
+    const percChance = response.body.current.percip
+    const feelsLike = response.body.current.feelslike
+    console.log(response.body.current.weather_descriptions[0] + `. It is currently ${temp} degrees out. It feels like ${feelsLike} degrees out.`)
+    console.log(`There is a ${percChance} percent chance of rain.`)
 })
