800c4511efe7dade4f799e9a8b304700c62ddf6d
Mitchell Goodwin <mgoodwin777@gmail.com>
2020-04-28 16:35:21 -0400

    Added geocoding

--- app.js
+++ app.js
@@ -7,5 +7,12 @@ request({ url: url, json: true }, (error, response) => {
     const percChance = response.body.current.percip
     const feelsLike = response.body.current.feelslike
     console.log(response.body.current.weather_descriptions[0] + `. It is currently ${temp} degrees out. It feels like ${feelsLike} degrees out.`)
-    console.log(`There is a ${percChance} percent chance of rain.`)
+})
+
+const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWl0Y2hlbGxnb29kd2luIiwiYSI6ImNrOWtjcGZ4bzBnZmUzZXBwYWtiMXVlNjAifQ.2ifp-FJvq-wBOGXZ15hLfQ&limit=1'
+
+request({url: geoURL, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[0]
+    console.log(latitude, longitude)
 })
